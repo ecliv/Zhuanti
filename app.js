ea20b2ae58f4c6a5c0f97b1be66c0b0de222dd33
Ferico Samuel <ferico.samuel@tokopedia.com>
2022-01-29 15:09:08 +0700

    use dotenv

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var bannerRouter = require('./routes/banner')
+require('dotenv').config();
 
 var app = express();
 
