a41c25872db38ab12d7fd3003d4783c7f827a5ca
Ferico Samuel <ferico55@gmail.com>
2022-02-28 18:01:01 +0700

    adjust for the category filter

--- repository/productRepository.js
+++ repository/productRepository.js
@@ -11,72 +11,84 @@ class ProductRepository {
             if (err != null) {
                 valueCallback([])
             } else {
-                let result = []
-                let lastRow = null
-                for (const index in rows) {
-                    const row = rows[index]
-                    console.log(row)
-                    if (!!lastRow && row.id === lastRow.id) {
-                        lastRow.children.push({
-                            id: row.child_id,
-                            name: row.child_name,
-                            image_url: row.child_image_url,
-                            price: row.child_price,
-                            description: row.child_description,
-                            weight: row.child_weight,
-                            stock: row.child_stock,
-                        })
-                    } else {
-                        if (!!lastRow) {
-                            console.log(lastRow)
-                            result.push(lastRow)
-                        }
-                        
-                        let product = {
-                            id: row.id,
-                            category_id: row.category_id,
-                            name: row.name,
-                            image_url: row.image_url,
-                            price: row.price,
-                            description: row.description,
-                            weight: row.weight,
-                            stock: row.stock,
-                            category_name: row.category_name,
-                            children: []
-                        }
-
-                        if (!!row.child_id) {
-                            let child = {
-                                id: row.child_id,
-                                name: row.child_name,
-                                image_url: row.child_image_url,
-                                price: row.child_price,
-                                description: row.child_description,
-                                weight: row.child_weight,
-                                stock: row.child_stock,
-                            }
-                            product.children.push(child)
-                        }
-                        lastRow = product
-                    }
-                }
-                if (!!lastRow) {
-                    console.log(lastRow)
-                    result.push(lastRow)
-                }
+                const result = this.processProductData(rows)
                 valueCallback(result)
             }
         })
     }
 
     getProductsWithCategory(categoryId, valueCallback) {
-        connection.query("select products.id, category_id, products.name, image_url, price, description, weight, stock, categories.name as category_name from products JOIN categories on products.category_id = categories.id where products.category_id = ?", [categoryId], (err, rows, fields) => {
+        connection.query(`select products.id, products.category_id, products.name, products.image_url, products.price, products.description, products.weight, products.stock, categories.name as category_name,
+        v.id as child_id, v.name as child_name, v.price as child_price, v.image_url as child_image_url, v.description as child_description, v.weight as child_weight, v.stock as child_stock
+            from products 
+            JOIN categories on products.category_id = categories.id
+            LEFT JOIN products v on v.parent_id = products.id
+            where products.parent_id is null and products.category_id = ?`, [categoryId], (err, rows, fields) => {
             if (err != null) {
                 valueCallback([])
             } else {
-                valueCallback(rows)
+                const result = this.processProductData(rows)
+                valueCallback(result)
             }
         })
+    }
+
+    processProductData(rows) {
+        let result = []
+        let lastRow = null
+        for (const index in rows) {
+            const row = rows[index]
+            console.log(row)
+            if (!!lastRow && row.id === lastRow.id) {
+                lastRow.children.push({
+                    id: row.child_id,
+                    name: row.child_name,
+                    image_url: row.child_image_url,
+                    price: row.child_price,
+                    description: row.child_description,
+                    weight: row.child_weight,
+                    stock: row.child_stock,
+                })
+            } else {
+                if (!!lastRow) {
+                    console.log(lastRow)
+                    result.push(lastRow)
+                }
+                
+                let product = {
+                    id: row.id,
+                    category_id: row.category_id,
+                    name: row.name,
+                    image_url: row.image_url,
+                    price: row.price,
+                    description: row.description,
+                    weight: row.weight,
+                    stock: row.stock,
+                    category_name: row.category_name,
+                    children: []
+                }
+
+                if (!!row.child_id) {
+                    let child = {
+                        id: row.child_id,
+                        name: row.child_name,
+                        image_url: row.child_image_url,
+                        price: row.child_price,
+                        description: row.child_description,
+                        weight: row.child_weight,
+                        stock: row.child_stock,
+                    }
+                    product.children.push(child)
+                }
+                lastRow = product
+            }
+        }
+        if (!!lastRow) {
+            console.log(lastRow)
+            result.push(lastRow)
+        }
+
+        return result
     }
 
     deleteProduct(id) {
