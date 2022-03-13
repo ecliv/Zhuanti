fc50f791ef3f106fe4a4d4d6111c8a0e533dae1f
Ferico Samuel <ferico55@gmail.com>
2022-03-13 21:33:31 +0700

    get orders done

--- repository/orderRepository.js
+++ repository/orderRepository.js
@@ -12,6 +12,79 @@ class OrderRepository {
         const query = `insert into order_items values(NULL, ?, ?, ?)`
         connection.query(query, [orderId, productId, qty])
     }
+
+    getOrdersForUser(userId, valueCallback) {
+        const query = `select o.id, o.total, o.created, o.status, o.note, a.id as address_id, a.alias, a.phone_number, a.address_line, a.postal_code, 
+            p.id as product_id, p.name, parent.name as parent_name, p.image_url, p.price, p.description, i.qty, p.price * i.qty as 'item_total' from orders o 
+        join addresses a on o.address_id = a.id 
+        join order_items i on o.id = i.order_id
+        join products p on i.product_id = p.id
+        join products parent on p.parent_id = parent.id
+        where o.user_id = ?
+        order by o.created desc`
+        connection.query(query, [userId], (err, rows, fields) => {
+            if (err != null) {
+                valueCallback([])
+            } else {
+                let result = []
+                let lastRow = null
+
+                for (const index in rows) {
+                    const row = rows[index]
+                    if (!!lastRow && row.id === lastRow.id) {
+                        lastRow.items.push({
+                            id: row.product_id,
+                            name: row.name,
+                            parent_name: row.parent_name,
+                            image_url: row.image_url,
+                            price: row.price,
+                            description: row.description,
+                            total: row.item_total,
+                            qty: row.qty
+                        })
+                    } else {
+                        if (!!lastRow) {
+                            result.push(lastRow)
+                        }
+
+                        let item = {
+                            id: row.product_id,
+                            name: row.name,
+                            parent_name: row.parent_name,
+                            image_url: row.image_url,
+                            price: row.price,
+                            description: row.description,
+                            total: row.item_total,
+                            qty: row.qty
+                        }
+
+                        const order = {
+                            items: [item],
+                            id: row.id,
+                            total: row.total,
+                            created: row.created,
+                            status: row.status,
+                            note: row.note,
+                            address: {
+                                id: row.address_id,
+                                alias: row.alias,
+                                phone_number: row.phone_number,
+                                address_line: row.address_line,
+                                postal_code: row.postal_code
+                            }
+                        }
+                        lastRow = order
+                    }
+                }
+
+                if (!!lastRow) {
+                    result.push(lastRow)
+                }
+
+                valueCallback(result)
+            }
+        })
+    }
 }
 
 module.exports = new OrderRepository()
