cea1b4be4e6b47202eba25ed5c6f137787f8be4c
Ferico Samuel <ferico55@gmail.com>
2022-04-02 10:39:42 +0700

    drinks menu

--- controller/botController.js
+++ controller/botController.js
@@ -1,7 +1,13 @@
 const categoryRepository = require('../repository/categoryRepository')
+const productRepository = require('../repository/productRepository')
+
+const formatter = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+});
 
 class BotController {
-    handleWebhook(req, res, next) {
+    handleWebhook = (req, res, next) => {
         const tag = req.body.queryResult.intent.displayName
         console.log(tag)
         let jsonResponse = {}
@@ -41,6 +47,37 @@ class BotController {
                 })
                 
                 break;
+            case "drinksmenu.list":
+                productRepository.getProductsWithCategory(1, (data) => {
+                    const response = this.constructMenuResponse(data)
+                    res.send(response)
+                })
+                break;
+        }
+    }
+
+    constructMenuResponse = (products) => {
+        const cards = products.map(product => {
+            return {
+                "card": {
+                    "title": `${product.name} - ${formatter.format(product.price)}`,
+                    "subtitle": product.description,
+                    "imageUri": product.image_url,
+                }
+            }
+        })
+
+        return {
+            "fulfillmentMessages": [
+                {
+                    "text": {
+                        "text": [
+                            "Here is the menu for Drinks:"
+                        ]
+                    }
+                },
+                ...cards
+            ]
         }
     }
 }
