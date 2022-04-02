7e4316b4c6e4c24f9d05b2efda82b240c07e0dd2
Ferico Samuel <ferico55@gmail.com>
2022-04-02 09:55:23 +0700

    test

--- controller/botController.js
+++ controller/botController.js
@@ -1,17 +1,32 @@
 class BotController {
     handleWebhook(req, res, next) {
         const tag = req.body.queryResult.intent.displayName
-        let jsonResponse = {
-            fulfillment_messages: [
-                {
-                    text: {
-                        text: ['My name is Flowhook'],
-                    },
-                },
-            ],
-        };
-
         console.log(tag)
+        let jsonResponse = {}
+
+        switch (tag) {
+            case "menu.list":
+                jsonResponse = {
+                    "fulfillmentMessages": [
+                        {
+                            "card": {
+                                "title": "card title",
+                                "subtitle": "card text",
+                                "imageUri": "https://example.com/images/example.png",
+                                "buttons": [
+                                    {
+                                        "text": "button text",
+                                        "postback": "https://example.com/path/for/end-user/to/follow"
+                                    }
+                                ]
+                            }
+                        }
+                    ]
+                }
+                break;
+        }
+
+        
         res.send(jsonResponse)
     }
 }
